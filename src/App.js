49f58a36452d1c08d9b1f03f629e2ae1ad928fa2
BrunoBisio <bisioba29@gmail.com>
2020-08-08 00:39:53 -0300

    carusel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {Container} from 'react';
 import logo from './logo.svg';
+import MyCarrousel from './components/myComponent.js';
+import SecondExample from './components/secondExample.js';
+import {Card, CardMedia} from '@material-ui/core';
 import './App.css';
 import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import SignInSide from './component/SignInSide.js'
@@ -9,19 +12,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
+        <SecondExample/>
       </header>
       <BrowserRouter>
           <Switch>
