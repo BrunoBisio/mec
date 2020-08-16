01cc5153826e7bca99a2b508a3adb45efffd637b
Alexis Sessarego <alexis.joel.sessarego@gmail.com>
2020-08-16 19:05:59 -0300

    Added configuration to the express server to deploy the react app.

--- server/app.js
+++ server/app.js
@@ -10,16 +10,21 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
+ //app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../build')));
+app.all('/', function(req, res, next) {
+  const route = __dirname + '/../build/index.html'
+  console.log(route)
+  res.sendFile(path.join(route));
+  //res.render('index', { title: 'Express' });
+});
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
