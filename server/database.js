a6d2a22ca922ca5a48ca581fbf4344b482b73442
Alexis Sessarego <alexis.joel.sessarego@gmail.com>
2020-08-23 01:45:31 -0300

    changes on database

--- server/database.js
+++ server/database.js
@@ -2,7 +2,13 @@ const { Sequelize } = require('sequelize');
 let connection;
 const scope = process.env.SCOPE
 if(scope === 'production') {
-  connection = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+  connection = new Sequelize(process.env.CLEARDB_DATABASE_URL, {
+    dialect: 'mysql',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+
+  });
 } else{
   connection = new Sequelize('MEC', 'mecadmin', 'root', {
     host: 'localhost',
