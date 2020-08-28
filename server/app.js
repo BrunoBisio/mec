55cbef99d4f89618124d8bd0cc4250a68989cf06
Alexis Sessarego <alexis.joel.sessarego@gmail.com>
2020-08-28 20:00:54 -0300

    added cors

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 // routes
 const usersRouter = require('./routes/users');
@@ -26,6 +27,7 @@ const passport = require('./middleware/passportMiddleware');
 let app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
