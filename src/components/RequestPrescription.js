37e75fc88352db224e21c91ca2ab1e04b4f24fe7
BrunoBisio <bisioba29@gmail.com>
2020-08-29 09:41:13 -0300

    prescription

--- src/components/RequestPrescription.js
+++ src/components/RequestPrescription.js
@@ -1,42 +1,83 @@
 import React from 'react';
-import { Typography, TextField, MenuItem, Paper, Button } from '@material-ui/core';
+import { Typography, TextField, MenuItem, Paper, Button, FormControl, InputLabel, Select } from '@material-ui/core';
 import { createPrescription } from '../services/PrescriptionsRepository.js';
 import '../css/styles/RequestPrescription.scss';
-
-const specialties = [
-    { id: 1, name: 'Adicciones' },
-    { id: 2, name: 'Clinica medica' },
-    { id: 3, name: 'Heridas Magicas' },
-    { id: 4, name: 'Nutricion' },
-    { id: 5, name: 'Psicologia' },
-    { id: 6, name: 'Traumatologia' }
-]
+import { getSpecialties } from '../services/DropDownRepositories.js';
+import { getLoggedUser } from '../services/RolRepository.js';
+import Axios from 'axios';
 
 class RequestPrescription extends React.Component {
 
     constructor(props) {
         super(props);
-
+        // if i get a user is because someone else is creating the prescription
         this.state = {
-            specialty: '',
-            description: '',
-            comment: ''
+            prescription: {
+                drug: '',
+                comment: '',
+                User: props.user,
+                Specialty: {},
+                date: new Date(),
+                UserId: props.user.id
+            },
+            close: props.onClose,
+            SpecialtyValue: '',
+            SpecialtyList: [],
+            SpecialtyLoaded: {},
         }
     }
 
-    handleSpecialtyChange = (event, obj) => { this.setState({ specialty: obj.props.value }); };
+    // select handlers
+    handleSelectChange = (event, obj) => {
+        const itemId = event.target.value;
+        const fieldName = event.target.name;
+        this.setState((state, props) => {
+            state.prescription[fieldName] = itemId > 0 ? state[fieldName + 'List'].find((item) => { return item.id = itemId }) : state[fieldName + 'Loaded'];
+            state.prescription[fieldName + 'Id'] = itemId > 0 ? state.prescription[fieldName].id : state[fieldName + 'Loaded'].id;
+            state[fieldName + 'Value'] = event.target.value;
+            return state;
+        });
+    }
 
-    handleDescriptionChange = (event, value) => { this.setState({ description: event.target.value }); };
+    handleTextChange = (event, value) => { 
+        const fieldValue = event.target.value;
+        const fieldName = event.target.name;
+        this.setState((state, props) => {
+            state.prescription[fieldName] = fieldValue;
+            return state;
+        });
+    }
 
-    handleCommentChange = (event, value) => { this.setState({ comment: event.target.value }); };
-
-    requesrPrescription = () => {
-        createPrescription(this.state);
+    requestPrescription = () => {
+        createPrescription(this.state.prescription);
         
-        this.setState({
-            specialty: '',
-            description: '',
-            comment: ''
+        this.setState((state, props) => {
+            state.close();
+            return state;
+        });
+    }
+
+    componentDidMount() {
+        Axios.all([
+            getLoggedUser(),
+            getSpecialties()
+        ]).then((responses) => {
+            if (responses[0].id !== this.state.prescription.User.id) {
+                this.setState((state, props) => {
+                    const SpecialtyLoaded = responses[0].Specialty ? responses[0].Specialty : {};
+                    const SpecialtyList = responses[1].data.filter((item) => { return item.id !== SpecialtyLoaded.id });
+                    state.SpecialtyLoaded = SpecialtyLoaded;
+                    state.SpecialtyList = SpecialtyList;
+                    return state;
+                });
+            } else {
+                this.setState((state, props) => {
+                    state.SpecialtyLoaded = null;
+                    state.SpecialtyList = responses[1].data;
+                    return state;
+                });
+            }
+            
         })
     }
 
@@ -51,18 +92,23 @@ class RequestPrescription extends React.Component {
                 </div>
                 <div className="InputContainer">
                     <div className="DetailsContainer">
-                        <TextField label="especialidad medica" select value={this.state.specialty} onChange={this.handleSpecialtyChange}>{
-                            specialties.map((option, index) => (
-                                <MenuItem key={index} value={option.name}>{option.name}</MenuItem>
-                            ))}</TextField>
-                        <TextField label="Descripcion" value={this.state.description} onChange={this.handleDescriptionChange}></TextField>
+                        <FormControl>
+                            <InputLabel shrink>Especialidad medica</InputLabel>
+                            <Select name="Specialty" value={this.state.SpecialtyValue} onChange={this.handleSelectChange} displayEmpty >
+                                {this.state.SpecialtyLoaded && <MenuItem value="">{this.state.SpecialtyLoaded.name}</MenuItem>}
+                                {this.state.SpecialtyList && this.state.SpecialtyList.map((option, index) => (
+                                    <MenuItem key={option.id} value={option.id}>{option.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <TextField name="drug" label="Remedio solicitado" value={this.state.drug} onChange={this.handleTextChange}></TextField>
                     </div>
                     <div className="CommentContainer">
-                        <TextField label="Comentario" multiline rows={6} variant="outlined" value={this.state.comment} onChange={this.handleCommentChange} ></TextField>
+                        <TextField name="comment" label="Comentario" multiline rows={6} variant="outlined" value={this.state.comment} onChange={this.handleTextChange} ></TextField>
                     </div>
                 </div>
                 <div>
-                    <Button onClick={this.requesrPrescription} variant="contained" color="primary" >Solicitar</Button>
+                    <Button onClick={this.requestPrescription} variant="contained" color="primary" >Solicitar</Button>
                 </div>
             </Paper>
         )
